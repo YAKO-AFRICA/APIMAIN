4c7f5f05ed495f2d8fd120a0a8637ac1b4e69844
bruceyapo <bruceyapo2000@gmail.com>
2026-07-09 11:09:27 +0000

    api jeko ok

--- app/Services/EncaissementBisService.php
+++ app/Services/EncaissementBisService.php
@@ -65,6 +65,10 @@ class EncaissementBisService
             return $this->failure('Aucune information trouvée pour ce contrat.');
         }
 
+        if ($details['OnStdbyOff'] == "3") {
+            return $this->failure('Ce contrat est arreté donc Impossible d’éffectuer un paiement !');
+        }
+
         $assures = $data['assures'] ?? [];
         $fraisAdhesion = 0;
         foreach ($assures as $garantie) {

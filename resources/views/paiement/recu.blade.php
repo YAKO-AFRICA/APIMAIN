210eb8dde7c50b7da115763d124f756e72892eb1
bruceyapo <bruceyapo2000@gmail.com>
2026-07-10 18:29:44 +0000

    api jeko ok

--- resources/views/paiement/recu.blade.php
+++ resources/views/paiement/recu.blade.php
@@ -357,13 +357,6 @@
             </svg>
             Imprimer / Télécharger
         </button>
-        <!-- par besoin -->
-        <!-- <a href="{{ route('dashboard') }}" class="btn btn-secondary">
-            <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                <path d="M19 12H5M12 19l-7-7 7-7"/>
-            </svg>
-            Retour
-        </a> -->
     </div>
 
     <!-- Footer -->
